51d024787a17bbcea0f6d0231409f48e4c96cad6
pylasrenu <pylasrenu@gmail.com>
2015-11-02 21:13:50 +0530

    add login project

---
+++ login/js/script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+		
+			$('#example').dataTable( {
+				
+				"info":     false,
+				"scrollY":  "360px",
+				"scrollCollapse": true,
+				"dom": '<"toolbar">frtip'
+			} );
+			$('#viewRolesData').dataTable( {
+				
+				"info":     false,
+				"scrollY":  "360px",
+				"scrollCollapse": true,
+				"dom": '<"toolbar">frtip'
+			} );
+			$('#viewMasterPropertiesTableData').dataTable( {
+				
+				"info":     false,
+				"scrollY":  "360px",
+				"scrollCollapse": true,
+				"dom": '<"toolbar">frtip'
+			} );
+			
+			
+			
+		} );
